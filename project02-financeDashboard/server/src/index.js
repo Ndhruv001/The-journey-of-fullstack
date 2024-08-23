01e34c3c44b4a1ac8d6440f74fe3d191f37a133a
nobita <nobitagroup001@gmail.com>
2024-08-23 22:52:08 +0530

    remove some empty lines

--- project02-financeDashboard/server/src/index.js
+++ project02-financeDashboard/server/src/index.js
@@ -1,10 +1,9 @@
 import app from "./app.js";
 import config from "../config/config.js";
 
-//CONFIGURATIONS
+
 
 //START THE SERVER
-
 app.listen(config.port, () => {
   console.log(`Server is running on port ${config.port}`);
 });
