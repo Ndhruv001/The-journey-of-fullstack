79048ca70b5baff45422b71d0f9ab08f79ded6b9
nobita <nobitagroup001@gmail.com>
2024-08-31 18:18:27 +0530

    add title Consent & Submit in doctor registration form

--- project04-carePlus/client/src/scenes/registration/doctor-registration/pages/ConsentAndPolicy.jsx
+++ project04-carePlus/client/src/scenes/registration/doctor-registration/pages/ConsentAndPolicy.jsx
@@ -1,14 +1,21 @@
 function ConsentAndPolicy() {
-    return (
-      <div className="mb-4">
-        <label className="inline-flex items-center">
-          <input type="checkbox" className="form-checkbox text-blue-600" />
-          <span className="ml-2 text-gray-700">
-            I agree to the terms and conditions and privacy policy
-          </span>
-        </label>
-      </div>
-    );
+  return (
+    <>
+      <h2 className="text-2xl font-bold text-center text-black mb-6">
+        Consent & Submit
+      </h2>
+      <form>
+        <div className="mb-4">
+          <label className="inline-flex items-center">
+            <input type="checkbox" className="form-checkbox text-blue-600" />
+            <span className="ml-2 text-gray-700">
+              I agree to the terms and conditions
+            </span>
+          </label>
+        </div>
+      </form>
+    </>
+  );
   }
   
   export default ConsentAndPolicy;
